08bce181d7b21a3ee910e08493cc0d494adc79d9
Erik Omlid <erik.omlid@announcebase.lan.announcemedia.com>
2014-03-02 19:12:09 -0600

    Added link add / get methods

---
+++ src/Punn/Link/Facades/Link.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Facade
+
+class Link extends Facade
+{
+	protected static function getFacadeAccessor() { return 'link'; }
+}
--- src/Punn/Link/Link.php
+++ src/Punn/Link/Link.php
@@ -2,7 +2,55 @@
 
 namespace Punn\Link;
 
+/**
+ * Link helper class for generating URLs (relative and absolute)
+ *
+ * @author Erik Omlid <erik@madfeller.com>
+ * @version 1.0.0
+ * @link https://github.com/punn/link
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ */
+
 class Link
 {
-	
+	private $links;
+
+	public function add($name, $link)
+	{
+		if(!is_array($this->links))
+		{
+			$this->links = array();
+		}
+		$this->links = array_add($this->links, $name, $link);
+	}
+
+	public function get($name, $params = null, $full = true)
+	{
+		$url = '';
+		if(isset($this->links[$name]))
+		{
+			$url = $this->links[$name];
+		}
+
+		if(!empty($url))
+		{
+			if(count($params) > 0)
+			{
+				foreach($params as $key => $val)
+				{
+					$url = str_replace("[" . strtoupper($key). "]", $val, $url);
+				}
+			}
+
+			$url = preg_replace('/\[.*\]/', '', $url);
+			if($full && !preg_match('/^http/', $url))
+			{
+				$url = url('/') . $url;
+			}
+
+			return $url;
+		}
+
+		return "#";
+	}
 }
